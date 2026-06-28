cae8093abc79b119c2fa11393821f7814f744984
m-ouchi4 <m-ouchi4@meitec-grp.com>
2026-06-28 15:34:07 +0900

    リファクタリング

--- SaveImage.py
+++ SaveImage.py
@@ -5,5 +5,6 @@ class SaveImage:
     save_count = 1
 
     def save(savefolder, savepath, image):
+        os.makedirs(savefolder, exist_ok=True)
         cv2.imwrite(os.path.join(savefolder, f"{SaveImage.save_count}_{savepath}"), image)
         SaveImage.save_count += 1
